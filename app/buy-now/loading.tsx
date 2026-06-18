3fbbe425344ad2236e78eb37d2291f3eef2afbcd
AY0OLA <ayoolaokunow@gmail.com>
2026-06-18 07:59:09 +0100

    commit

--- app/buy-now/loading.tsx
+++ app/buy-now/loading.tsx
@@ -1,4 +1,6 @@
 const Loading = () => {
+
+  
   return (
     <div className="flex flex-col mx-auto justify-center items-center h-[70vh]">
       <div className="animate-spin rounded-full h-20 w-20 border-4 border-t-[#043033] border-gray-200 self-center"></div>
