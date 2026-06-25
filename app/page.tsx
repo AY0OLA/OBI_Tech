01ebf92eb1b812f3f15cbae12979bb9b0c480cc8
AY0OLA <ayoolaokunow@gmail.com>
2026-06-25 18:20:54 +0100

    adjust prodct

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import { Navbar } from "@/component/Navbar";
 import { fetchProducts } from "@/utils/action/product.action";
 import React from "react";
 
-const allProducts = await fetchProducts();
 export default async function Page() {
+  const allProducts = await fetchProducts();
   
   return (
     <div>
